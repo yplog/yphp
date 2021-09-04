9f22fcb9b503857fbfbf855aea69e68d8f9e05a2
yp <yp@pop-os.localdomain>
2021-09-04 11:01:34 +0300

    np

--- components/headArea.js
+++ components/headArea.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Head from "next/head";
 
-export default function HeadArea({ title="yp's home page" }) {
+export default function HeadArea({ title = "yp's home page" }) {
   return (
     <Head>
-        <title>{title}</title>
-        <meta charset="UTF-8" />
-        <meta name="description" content="Yalın Pala's Home Page" />
-        <meta name="keywords" content="Yalın Pala, Software Developer" />
-        <meta name="author" content="Yalın Pala" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <title>{title}</title>
+      <meta charSet="UTF-8" />
+      <meta name="description" content="Yalın Pala's Home Page" />
+      <meta name="keywords" content="Yalın Pala, Software Developer" />
+      <meta name="author" content="Yalın Pala" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
   );
 }
