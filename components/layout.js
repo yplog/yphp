3f6148c0c187b67d7fce040a51a7e5de31c17541
Yalin Pala <yalinpala@protonmail.com>
2021-11-17 08:19:58 +0300

    lang en

--- components/layout.js
+++ components/layout.js
@@ -1,22 +1,20 @@
-import React from 'react'
-import HeadArea from './headArea'
-import Header from './header'
-import Footer from './footer'
+import React from "react";
+import HeadArea from "./headArea";
+import Header from "./header";
+import Footer from "./footer";
 import styles from "../styles/Home.module.css";
 
 export default function Layout({ children }) {
-	return (
-		<div className={styles.container}>
-			<HeadArea />
+  return (
+    <div className={styles.container}>
+      <HeadArea />
       <Header />
 
-			<section className={styles.content}>
-				{children}
-			</section>
-			
-			<hr />
+      <section className={styles.content}>{children}</section>
 
-			<Footer />
-		</div>
-	)
+      <hr />
+
+      <Footer />
+    </div>
+  );
 }
