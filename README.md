479e166fdbe8dc0d2a968b65bbc1a1e5c5890b77
yplog <yalinpala@protonmail.com>
2022-05-16 14:42:34 +0300

    512kb club

--- README.md
+++ README.md
@@ -4,5 +4,13 @@
 ![Lighthouse Report](https://raw.githubusercontent.com/yplog/yphp/main/public/images/Lighthouse_Report_2021-11-17%2008-37-09.png)
 
 
+## GTmetrix Report
+[GTmetrix Report](https://gtmetrix.com/reports/yalinpala.dev/6ThadtmG/)
+
+## 512kb.club
+[512kb.club](https://512kb.club/)
+
+![512kb](https://512kb.club/assets/images/blue-team.svg)
+
 ## CV
-![JSON Resume](https://jsonresume.org/)
+[JSON Resume](https://jsonresume.org/)
--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <Link href="https://512kb.club">
+        <a target="_blank" rel="noreferrer">
+          <img src="https://512kb.club/assets/images/blue-team.svg" />
+        </a>
+      </Link>
       <Image
-          src="/images/wtfpl-badge-1.png"
-          alt="The Sandman 1989 Issue 18"
-          height={31}
-          width={88}
-          className={styles.resImg}
-        />
-        <Image
-          src="/images/262323.png"
-          alt="The Sandman 1989 Issue 18"
-          height={31}
-          width={88}
-          className={styles.resImg}
-        />
-      </footer>
+        src="/images/wtfpl-badge-1.png"
+        alt="wtfpl"
+        height={31}
+        width={88}
+        className={styles.resImg}
+      />
+      <Image
+        src="/images/262323.png"
+        alt="The Sandman 1989 Issue 18"
+        height={31}
+        width={88}
+        className={styles.resImg}
+      />
+    </footer>
   );
 }
