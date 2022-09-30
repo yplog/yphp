4a0d786603ec382f632b7ec679f04141864aac61
yplog <yalinpala@protonmail.com>
2022-09-30 11:26:59 +0300

    updated footer links

--- components/footer.js
+++ components/footer.js
@@ -6,20 +6,28 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Image
-        src="/images/262323.png"
-        alt="Free Software Foundation"
-        height={31}
-        width={88}
-        className={styles.resImg}
-      />
-      <Image
-        src="/images/wtfpl-badge-1.png"
-        alt="wtfpl"
-        height={31}
-        width={88}
-        className={styles.resImg}
-      />
+      <Link href="https://my.fsf.org/join">
+        <a target="_blank" rel="noreferrer">
+          <Image
+            src="/images/262323.png"
+            alt="Free Software Foundation"
+            height={45}
+            width={180}
+            className={styles.resImg}
+          />
+        </a>
+      </Link>
+      <Link href="http://www.wtfpl.net/">
+        <a target="_blank" rel="noreferrer">
+          <Image
+            src="/images/wtfpl-badge-1.png"
+            alt="wtfpl"
+            height={31}
+            width={88}
+            className={styles.resImg}
+          />
+        </a>
+      </Link>
       <Link href="https://512kb.club">
         <a target="_blank" rel="noreferrer">
           <Image
