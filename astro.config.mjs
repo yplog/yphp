102537a22ff9fbc9c34bcce8f7ba7e08d030c855
yplog <yalinpala@protonmail.com>
2024-10-29 22:06:27 +0300

    fix(config): experimental

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
+  // experimental: {
+  //   assets: true,
+  // },
 });
