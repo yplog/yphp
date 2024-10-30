dd374ed65f4b69ae5bad5c4c4bc1f85af6eef272
yplog <yalinpala@protonmail.com>
2024-10-30 23:27:26 +0300

    init v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  // experimental: {
-  //   assets: true,
-  // },
-});
+export default defineConfig({});
