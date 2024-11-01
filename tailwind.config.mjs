81bfacd341964ac7a9799b0cc1d5e661c491f8c6
yplog <yalinpala@protonmail.com>
2024-11-01 15:45:41 +0300

    feat(font): add roboto & navigation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -8,6 +10,9 @@ export default {
 				'h1-text': '#d4d4d6',
 				'h2-text': '#aaaaaa',
 				'content-text': '',
+			},
+			fontFamily: {
+				'sans': ['"Roboto"', ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
